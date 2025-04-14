0dcb1f5ec90342c72770efac0d9c8f88b6c06d3a
Deepon Das <deeponsubhro@gmail.com>
2025-04-14 16:43:56 +0530

    This is the app which will help us to collect the data

---
+++ app/src/main/java/com/example/dr/screen/BrushBottomSheet.kt
@@ -0,0 +1,232 @@
+package com.example.dr.screen
+
+
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.content.Intent
+import android.net.Uri
+import com.example.dr.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BrushBottomSheet(
+    context: Context,
+    onDismiss: () -> Unit,
+    drawingView: DrawingView?,
+    colors: List<Color>,
+    backgroundColors: List<Color>,
+    currentBrushSize: Float,
+    currentColorIndex: Int, // Keep track of the current brush color index
+    currentBackgroundColorIndex: Int, // Keep track of the current background color index
+    changeBrushSize: (Float) -> Unit,
+    changeBrushColor: (Color) -> Unit,
+    changeBackgroundColor: (Color) -> Unit,
+    updateCurrentColorIndex: (Int) -> Unit, // New parameter to update the color index
+    updateCurrentBackgroundColorIndex: (Int) -> Unit // New parameter to update the background color index
+) {
+    ModalBottomSheet(onDismissRequest = onDismiss) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            // Brush Size Selector
+            Text(
+                text = "Select Brush Size",
+                fontSize = 18.sp,
+                color = Color.DarkGray,
+                modifier = Modifier.padding(start = 9.dp, top = 14.dp)
+            )
+            Slider(
+                value = currentBrushSize,
+                onValueChange = { size -> changeBrushSize(size) },
+                valueRange = 2f..18f,
+                steps = 16 // This allows the slider to jump by integer values
+            )
+            Spacer(modifier = Modifier.height(9.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
+                    .background(colorResource(id = R.color.lightGray))
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                // Brush Color Selection
+                Text(
+                    text = "Select Brush Color",
+                    fontSize = 18.sp,
+                    color = Color.DarkGray,
+                    modifier = Modifier.padding(start = 9.dp, top = 14.dp)
+                )
+                colors.forEachIndexed { index, color ->
+                    ColorButton(
+                        buttonNumber = index,
+                        isSelected = index == currentColorIndex,
+                        colors = colors,
+                        onColorSelected = { buttonIndex ->
+                            changeBrushColor(colors[buttonIndex])
+                            updateCurrentColorIndex(buttonIndex) // Update current color index
+                        }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(9.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
+                    .background(colorResource(id = R.color.lightGray))
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                // Background Color Selection
+                Text(
+                    text = "Select Background Color",
+                    fontSize = 18.sp,
+                    color = Color.DarkGray,
+                    modifier = Modifier.padding(start = 9.dp, top = 14.dp)
+                )
+                backgroundColors.forEachIndexed { index, color ->
+                    ColorButton(
+                        buttonNumber = index,
+                        isSelected = index == currentBackgroundColorIndex,
+                        colors = backgroundColors,
+                        onColorSelected = { buttonIndex ->
+                            changeBackgroundColor(backgroundColors[buttonIndex])
+                            updateCurrentBackgroundColorIndex(buttonIndex) // Update current background color index
+                        }
+                    )
+                }
+            }
+
+            // Bottom bar with buttons like Undo, Redo, Clear, Brush Size, Change Background
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                IconButton(onClick = { drawingView?.onClickUndo() }) {
+                    Icon(
+                        painter = painterResource(R.drawable.undo),
+                        contentDescription = "Undo",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+                IconButton(onClick = { drawingView?.onClickRedo() }) {
+                    Icon(
+                        painter = painterResource(R.drawable.undo),
+                        contentDescription = "Redo",
+                        tint = Color.Unspecified,
+                        modifier = Modifier
+                            .size(32.dp)
+                            .graphicsLayer(rotationY = 180f)
+                    )
+                }
+                IconButton(onClick = { drawingView?.clearCanvas() }) {
+                    Icon(
+                        painter = painterResource(R.drawable.clear),
+                        contentDescription = "Clear",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        drawingView?.let { view ->
+                            val bitmap = view.getBitmap(backgroundColors[currentBackgroundColorIndex]) // Pass the current background color
+                            val uri =
+                                drawingView.saveBitmap(context, bitmap) // Save the bitmap and get the URI
+                            if (uri != null) {
+                                shareBitmap(context, uri) // Share the saved bitmap
+                            }
+                        }
+                    },
+                    modifier = Modifier.size(56.dp) // Set the size for the IconButton
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.share),
+                        contentDescription = "Share Drawing",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = painterResource(R.drawable.ai),
+                        contentDescription = "Eraser",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+private fun shareBitmap(context: Context, uri: Uri) {
+    val shareIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_STREAM, uri)
+        type = "image/png"
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
+    context.startActivity(Intent.createChooser(shareIntent, "Share Drawing"))
+}
+
+/*
+private fun saveBitmapAndShare(context: Context, bitmap: Bitmap) {
+    val file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Drawing_${System.currentTimeMillis()}.png")
+
+    try {
+        FileOutputStream(file).use { out ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            out.flush()
+        }
+
+        // Share the image
+        val uri: Uri = FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, uri)
+            type = "image/png"
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        context.startActivity(Intent.createChooser(shareIntent, "Share Drawing"))
+    } catch (e: Exception) {
+        Toast.makeText(context, "Error sharing drawing: ${e.message}", Toast.LENGTH_SHORT).show()
+    }
+}*/
+
+
---
+++ app/src/main/java/com/example/dr/screen/DrawingView.kt
@@ -0,0 +1,227 @@
+package com.example.dr.screen
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.net.Uri
+import android.os.Environment
+import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.FileProvider
+import com.example.dr.TFLiteClassifier
+import com.example.dr.getProcessedFeatures
+import java.io.File
+import java.io.FileOutputStream
+
+class DrawingView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 7.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    // Paths to store drawn lines
+    private val mPaths = ArrayList<CustomPath>()
+    private val mUndoPaths = ArrayList<CustomPath>()
+    private val mRedoPaths = ArrayList<CustomPath>() // New Redo paths list
+
+    // Variable to store all coordinates from mPaths
+    private val allCoordinates = mutableListOf<List<Triple<Int, Int, Int>>>()
+
+    init {
+        setUpDrawing()
+    }
+
+    // Function to update the coordinates list
+    private fun updateAllCoordinates() {
+        allCoordinates.clear()
+        allCoordinates.addAll(mPaths.map { it.coordinates })
+    }
+
+    // Undo functionality
+    fun onClickUndo() {
+        if (mPaths.isNotEmpty()) {
+            mUndoPaths.add(mPaths.removeAt(mPaths.size - 1))
+            mRedoPaths.clear()
+            updateAllCoordinates() // Update after undo
+            invalidate()
+        }
+    }
+
+    // Redo functionality
+    fun onClickRedo() {
+        if (mUndoPaths.isNotEmpty()) {
+            mPaths.add(mUndoPaths.removeAt(mUndoPaths.size - 1))
+            updateAllCoordinates() // Update after redo
+            invalidate()
+        }
+    }
+
+    // Clear the canvas
+    fun clearCanvas() {
+        mPaths.clear()
+        mUndoPaths.clear()
+        mRedoPaths.clear()
+        updateAllCoordinates() // Update after clear
+        invalidate()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        for (path in mPaths) {
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPaint!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x?.toInt() ?: return false
+        val touchY = event.y.toInt()
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX.toFloat(), touchY.toFloat())
+
+                mDrawPath!!.coordinates.add(Triple(touchX, touchY, 1))
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX.toFloat(), touchY.toFloat())
+                mDrawPath!!.coordinates.add(Triple(touchX, touchY, 1))
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath!!.coordinates.add(Triple(touchX, touchY, 0))
+                mPaths.add(mDrawPath!!)
+                updateAllCoordinates() // Update after new path
+                Log.d("DrawingView", "Updated allCoordinates: $allCoordinates")
+
+                val features = getProcessedFeatures(this) // Ensure this returns a FloatArray
+                Log.d("ProcessedFeatures", "Extracted Features: $features")
+                Log.d("ProcessedFeatures","Extracted Features List Size: ${features.size}")
+
+                /*
+                val classifier = TFLiteClassifier(context)
+                try {
+                    val predictedClassIndex = classifier.predict(features)
+                    val labelClasses = arrayOf(
+                        "A/अ/অ","AA/आ/আ","ADA/ढ़/ঢ","AN/ं/ং",
+                        "BA/ब/ব","BHA/भ/ভ","BI/ः/ঃ",
+                        "C/च/চ","CH/छ/ছ","CN/ँ/ঁ",
+                        "DA/द/দ","DDA/ड/ড","DDH/ढ/ঢ","DHA/ध/ধ","DRA/ड़/ড়",
+                        "E/ए/এ","EN/ञ/ঞ",
+                        "G/ग/গ","GH/घ/ঘ",
+                        "HA/ह/হ",
+                        "I/इ/ই","II/ई/ঈ",
+                        "JA/ज/জ","JH/झ/ঝ",
+                        "K/क/ক","KH/ख/খ","KT/त्/ৎ",
+                        "LA/ल/ল",
+                        "MA/म/ম","MN/ण/ণ","MSA/ष/ষ",
+                        "NA/न/ন",
+                        "O/ओ/ও","OI/ऐ/ঐ","OU/औ/ঔ",
+                        "PA/प/প","PHA/फ/ফ",
+                        "RA/र/র","RI/ऋ/ঋ",
+                        "S/स/স","SA/श/শ",
+                        "T/ट/ট","TA/त/ত","THA/थ/থ","TTA/ठ/ঠ",
+                        "U/उ/উ","UN/ङ/ঙ","UU/ऊ/ঊ",
+                        "Y/य़/য়","YA/य/য"
+                    )
+                    val predictedChar = labelClasses[predictedClassIndex]
+                    Log.d("Prediction", "Predicted Class Index: $predictedClassIndex")
+                    Log.d("Prediction", "Predicted Character: $predictedChar")
+                    Toast.makeText(context, "Predicted Character: $predictedChar", Toast.LENGTH_LONG).show()
+                } catch (e: IllegalArgumentException) {
+                    Log.e("TFLiteClassifier", "Prediction failed: ${e.message}")
+                    Toast.makeText(context, "Draw a complete character and try again", Toast.LENGTH_SHORT).show()
+                }
+                 */
+
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+        }
+        invalidate()
+        return true
+    }
+
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    // Set color method
+    fun setColor(currentColor: String) {
+        color = Color.parseColor(currentColor)
+        mDrawPaint!!.color = color // Update your paint object if you have one
+        invalidate() // Redraw the view
+    }
+
+    fun getAllCoordinates(): List<List<Triple<Int, Int, Int>>> {
+        return allCoordinates
+    }
+
+    fun getBitmap(currentBackgroundColor: androidx.compose.ui.graphics.Color): Bitmap {
+        // Create a bitmap with the same size as the drawing view
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        // Create a canvas to draw onto the bitmap
+        val canvas = Canvas(bitmap)
+
+        // Draw the background color
+        canvas.drawColor(currentBackgroundColor.toArgb()) // Use the current background color
+
+        // Draw the current drawing onto the bitmap
+        draw(canvas)
+
+        return bitmap
+    }
+
+    fun saveBitmap(context: Context, bitmap: Bitmap): Uri? {
+        val file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Drawing_${System.currentTimeMillis()}.png")
+        return try {
+            FileOutputStream(file).use { out ->
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+                out.flush()
+            }
+            // Return the Uri for the saved file
+            FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+        } catch (e: Exception) {
+            Toast.makeText(context, "Error saving drawing: ${e.message}", Toast.LENGTH_SHORT).show()
+            null
+        }
+    }
+
+}
